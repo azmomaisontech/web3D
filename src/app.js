a844e44f36d953291c9c4b3f11d83b5a483184ab
Moses <mosesaizee@outlook.com>
2022-04-09 11:20:15 +0100

    feat: basic scene and box with r3f

--- src/app.js
+++ src/app.js
@@ -1,12 +1,30 @@
-import './app.css';
 import React from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./app.css";
+
+function Box() {
+  const meshRef = React.useRef();
+  useFrame(() => {
+    if (!meshRef.current) return;
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += 0.01;
+  });
+  return (
+    <mesh ref={meshRef}>
+      <boxBufferGeometry />
+      <meshBasicMaterial color="blue" />
+    </mesh>
+  );
+}
 
 export default function App() {
   return (
-      <React.StrictMode>
-          <div className="App">
-              <h1>First web3D</h1>
-          </div>
-      </React.StrictMode>
+    <React.StrictMode>
+      <div className="app">
+        <Canvas style={{ background: "#222222" }}>
+          <Box />
+        </Canvas>
+      </div>
+    </React.StrictMode>
   );
 }
