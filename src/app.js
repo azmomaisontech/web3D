a57fd9f102e41ec6ea11741998c36e8c84be085c
Moses <mosesaizee@outlook.com>
2022-04-21 12:24:46 +0100

    feat: creating points to a scene

--- src/app.js
+++ src/app.js
@@ -1,27 +1,41 @@
-import React from "react";
 import "./app.css";
-import { Canvas, useFrame, extend, useThree } from "@react-three/fiber";
+import React from "react";
+import { Canvas, extend, useFrame, useThree } from "@react-three/fiber";
+import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
 extend({ OrbitControls });
 
-function OrbitControl() {
+function CameraControl() {
   const { camera, gl } = useThree();
   return <orbitControls args={[camera, gl.domElement]} />;
 }
 
 function Box(props) {
-  const meshRef = React.useRef();
+  const ref = React.useRef();
   useFrame(() => {
-    if (!meshRef.current) return;
-    meshRef.current.rotation.x += 0.01;
-    meshRef.current.rotation.y += 0.01;
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
   });
+
   return (
-    <mesh ref={meshRef} {...props}>
+    <mesh ref={ref} {...props}>
       <boxBufferGeometry />
-      <meshBasicMaterial color="blue" />
+      <meshBasicMaterial color={"blue"} />
     </mesh>
   );
+}
+
+function Points({ count = 1 }) {
+  const vertices = new Float32Array(count * 3);
+  for (let x = 0; x < count * 3; x++) {
+    vertices[x] = 0;
+  }
+  const material = new THREE.PointsMaterial({ color: "white", size: 1 });
+  const geometry = new THREE.BufferGeometry();
+  geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
+
+  return <points material={material} geometry={geometry} />;
 }
 
 export default function App() {
@@ -29,12 +43,15 @@ export default function App() {
     <React.StrictMode>
       <div className="app">
         <Canvas
-          style={{ background: "#222222" }}
+          style={{ backgroundColor: "#222222" }}
           camera={{ position: [3, 3, 3] }}
         >
-          <OrbitControl />
-          <Box position={[1, 1, 0]} />
-          <axesHelper args={[5]} />
+          <React.Suspense fallback={null}>
+            <CameraControl />
+            <Box position={[0, 1, 1]} />
+            <Points />
+            <axesHelper args={[5]} />
+          </React.Suspense>
         </Canvas>
       </div>
     </React.StrictMode>
